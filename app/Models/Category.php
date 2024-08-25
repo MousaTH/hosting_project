96d5802c55b9540050000431a87b8159bfbdfe85
TbadolStation <tbadolstation2024@gmail.com>
2024-08-25 19:51:30 +0300

    .17

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $table = 'category';  // this will override the default 'categories' table name in the database.
     protected $fillable = ['category_name','category_icon'];
     public function products()
     {
