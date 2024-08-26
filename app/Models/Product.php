b95f56236b6f23296597ae6aceb7da5537be10f4
TbadolStation <tbadolstation2024@gmail.com>
2024-08-27 00:24:00 +0300

    .adduser_id3

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,7 +18,7 @@ class Product extends Model
    public function category(){
        return $this->belongsTo(Category::class);
    }
-   public function users(){
+   public function user(){
        return $this->belongsTo(User::class);
    }
 }
