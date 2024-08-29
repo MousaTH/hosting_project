f622de246b35a4b5c99a0b00b2dd15d790f64c0b
TbadolStation <tbadolstation2024@gmail.com>
2024-08-29 06:24:13 +0300

    guestShowProduct

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ Route::get("/profile",function (Request $request){
     //only authenticated user can access this api
 return response()->json(['msg'=>'nice you are authinticated']);
 })->middleware('auth');
-Route::get('/productShow',[ProductController::class,'index']);
+//Route::get('/productShow',[ProductController::class,'index']);
+Route::resource('/products',ProductController::class);
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post('/logout',[AuthController::class ,'logout'])->middleware('auth:sanctum')->name('logout');
-    Route::resource('/products',ProductController::class);
     Route::post('/favorites/{product}',[FavoriteController::class,'toggleFavorite']);
     Route::get('/favorites',[FavoriteController::class,'index']);
     Route::get('/showProfile/{id}',[AuthController::class,'show']);
