35330ba04c457a6dab46e818de9845707a88e3ab
Mohammad Alhanini <mohammad1alhanini@gmail.com>
2024-08-30 01:36:01 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ Route::post('/login',[AuthController::class ,'login'])->name('login');
 Route::get("/test",function (Request $request){
     return "api is working fine fuck adnan rap3";
 });
+Route::get('/productShow',[ProductController::class,'index']);
 Route::get("/profile",function (Request $request){
     //only authenticated user can access this api
 return response()->json(['msg'=>'nice you are authinticated']);
 })->middleware('auth');
-//Route::get('/productShow',[ProductController::class,'index']);
 Route::resource('/products',ProductController::class);
 Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::post('/logout',[AuthController::class ,'logout'])->middleware('auth:sanctum')->name('logout');
